0394456e6868931fc0823af660d8e7ce1c76e7ca
Sriram <sri.kmb@gmail.com>
2018-03-05 11:33:48 -0800

    Add spring-boot sample application
    Signed-off-by: Sriram <sri.kmb@gmail.com>

---
+++ samples/hello-spring-boot/README.md
@@ -0,0 +1,4 @@
+Hello, Spring Boot!
+===================
+
+This sample demonstrates a simple spring-boot application with a Kotlin based build script.
---
+++ samples/hello-spring-boot/src/main/kotlin/hello/HelloController.kt
@@ -0,0 +1,11 @@
+package hello
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello() = "Hello, Spring Boot!.."
+}
