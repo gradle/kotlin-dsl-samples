7699f0d25f53d8d18deb5f6de0168eafdc11e136
Paul Merlin <paul@gradle.com>
2018-06-14 10:03:14 +0200

    Minimal exemplar setup

--- subprojects/samples-tests/samples-tests.gradle.kts
+++ subprojects/samples-tests/samples-tests.gradle.kts
@@ -4,9 +4,14 @@ plugins {
     id("kotlin-library")
 }
 
+repositories {
+    maven(url = "https://repo.gradle.org/gradle/ext-releases-local")
+}
+
 dependencies {
     compile(project(":test-fixtures"))
     compile("org.xmlunit:xmlunit-matchers:2.5.1")
+    implementation("org.gradle:sample-check:0.1.0")
 }
 
 val customInstallation by rootProject.tasks
