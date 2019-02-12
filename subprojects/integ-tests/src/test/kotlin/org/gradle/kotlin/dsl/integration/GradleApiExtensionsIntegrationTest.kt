f50289826c82b8e1d8c6ed2aae8c8771c2552d5d
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2019-02-12 10:48:02 -0200

    Update expected extensions jar hash after Gradle version update
    Signed-off-by: Rodrigo B. de Oliveira <rodrigo@gradle.com>

--- subprojects/integ-tests/src/test/kotlin/org/gradle/kotlin/dsl/integration/GradleApiExtensionsIntegrationTest.kt
+++ subprojects/integ-tests/src/test/kotlin/org/gradle/kotlin/dsl/integration/GradleApiExtensionsIntegrationTest.kt
@@ -191,7 +191,7 @@ class GradleApiExtensionsIntegrationTest : AbstractPluginIntegrationTest() {
         val generatedJarHash = HashUtil.createHash(generatedExtensionsJarFromTestKitUserHome(), "MD5")
         assertThat(
             generatedJarHash.asZeroPaddedHexString(32),
-            equalTo("30f6c4e833395b258a763c9b238a275f")
+            equalTo("eada20184f016348b5422bad1bd636f2")
         )
     }
 
