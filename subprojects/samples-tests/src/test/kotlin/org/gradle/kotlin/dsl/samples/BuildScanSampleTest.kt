e778a3a03a7fb0ffeda64eed6009882a47a7f060
Paul Merlin <paul@gradle.com>
2019-02-13 09:41:21 +0100

    Cleanup repository leaving only samples and samples-tests
    Samples tests now use gradle/exemplar
    Most of them are external samples tests
    Except for when precise testing is necessary or unsupported by exemplar
    
    Samples wrapper update is now done via the regular :wrapper task
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/BuildScanSampleTest.kt
+++ subprojects/samples-tests/src/test/kotlin/org/gradle/kotlin/dsl/samples/BuildScanSampleTest.kt
@@ -1,13 +1,24 @@
 package org.gradle.kotlin.dsl.samples
 
-import org.gradle.kotlin.dsl.fixtures.canPublishBuildScan
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.not
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 
 class BuildScanSampleTest : AbstractSampleTest("build-scan") {
 
     @Test
-    fun `publishes build scan`() {
-        canPublishBuildScan()
+    fun `can publish build scans`() {
+        build("tasks", "--scan").apply {
+            assertThat(
+                output,
+                allOf(
+                    containsString("Publishing build scan..."),
+                    not(containsString("The build scan plugin was applied after other plugins."))
+                )
+            )
+        }
     }
 }
