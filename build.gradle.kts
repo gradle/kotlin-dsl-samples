34788285f23494f032ae3b9e0caefaeaa760fa6d
Paul Merlin <paul@gradle.com>
2019-02-13 14:22:38 +0100

    Refine :samplesWrappers
    by only overwriting if necessary and logging updated files
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.hash.HashUtil
+
 plugins {
     base
 }
@@ -9,8 +11,14 @@ tasks {
             val wrapperFiles = wrapper.get().run {
                 listOf(scriptFile, batchScript, jarFile, propertiesFile).associateBy { it.name }
             }
-            file("samples").walk().filter { it.isFile && it.name in wrapperFiles.keys }.forEach { sampleWrapperFile ->
-                wrapperFiles.getValue(sampleWrapperFile.name).copyTo(sampleWrapperFile, overwrite = true)
+            val hashes = wrapperFiles.mapValues { HashUtil.sha256(it.value) }
+            file("samples").walk().filter { it.isFile && it.name in wrapperFiles }.forEach { sampleWrapperFile ->
+                wrapperFiles.getValue(sampleWrapperFile.name).let { wrapperFile ->
+                    if (HashUtil.sha256(sampleWrapperFile) != hashes.getValue(sampleWrapperFile.name)) {
+                        logger.lifecycle("Updating ${sampleWrapperFile.relativeTo(rootDir)}")
+                        wrapperFile.copyTo(sampleWrapperFile, overwrite = true)
+                    }
+                }
             }
         }
     }
