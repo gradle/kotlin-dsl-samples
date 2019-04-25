c012858396f47ca72bd0bf2c9f31cfcc68a1782e
Paul Merlin <paul@gradle.com>
2019-04-25 12:21:40 +0200

    Fix source-control sample for Java 12
    we don't want any Java requirement in the sample as we run ci checks on multiple Java versions
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- samples/source-control/sample/build.gradle.kts
+++ samples/source-control/sample/build.gradle.kts
@@ -6,11 +6,6 @@ application {
     mainClassName = "samples.HelloWorld"
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
 dependencies {
     implementation("org.gradle.kotlin.dsl.samples.source-control:compute:latest.integration")
 }
