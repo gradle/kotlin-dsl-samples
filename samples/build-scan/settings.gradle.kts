a7f7d5121b4ccac4c133538acda50ff736d58efa
Paul Merlin <paul@gradle.com>
2020-03-31 13:13:47 +0200

    Update build scan sample for plugin >= 3.0
    Signed-off-by: Paul Merlin <paul@gradle.com>

---
+++ samples/build-scan/settings.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
